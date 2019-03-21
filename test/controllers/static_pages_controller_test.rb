f839cd7c81d7bf7494da4ab313838e62a0138352
Daichi Uchigashima <ssch5050k@yahoo.co.jp>
2019-03-21 07:39:21 +0000

    Make a basic User model (include secure passwords)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,8 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
-  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Insta clone"
   end
